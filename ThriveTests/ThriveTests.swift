98e12787d92eda9e8075ac47a5edf4464fa8a973
Ryan Dunn <rcdunn@umich.edu>
2020-09-11 22:28:18 -0400

    Initial Commit

---
+++ ThriveTests/ThriveTests.swift
@@ -0,0 +1,34 @@
+//
+//  ThriveTests.swift
+//  ThriveTests
+//
+//  Created by Ryan Dunn on 9/11/20.
+//  Copyright © 2020 Ryan Dunn. All rights reserved.
+//
+
+import XCTest
+@testable import Thrive
+
+class ThriveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
